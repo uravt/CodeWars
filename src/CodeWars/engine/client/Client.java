99a27ff3599c6e54a9470d2e7ccf5ad7c077726b
Jack <jackfriedman17@gmail.com>
2024-02-24 16:21:48 -0500

    graphics

--- src/CodeWars/engine/client/Client.java
+++ src/CodeWars/engine/client/Client.java
@@ -1,6 +1,16 @@
 package CodeWars.engine.client;
+import javax.swing.*;
 
 public class Client
 {
     TileMap map = new TileMap();
+    JFrame frame = new JFrame();
+
+    public Client()
+    {
+        frame = new JFrame();
+        frame.setSize(500, 600);
+        frame.setVisible(true);
+    }
+
 }
